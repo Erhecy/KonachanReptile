cc2d06b57ff154228b4e3396fa44ad8386ea6fe9
ERHECY <1981324730@qq.com>
2021-03-28 11:57:31 +0800

    新增进度显示

--- konachanVer-2.py
+++ konachanVer-2.py
@@ -1,7 +1,7 @@
 import requests
 import re
 import os
-
+import time
 
 def kogetli(page):
     print('开始获取第', page, '页的图片')
@@ -16,13 +16,6 @@ def kogetli(page):
     return imglistli
 
 
-def seve_img(img_url, file_name):
-    img = requests.get(img_url)
-    f = open(file_name + '.jpg', 'ab') 
-    f.write(img.content)
-    f.close()
-
-
 def get_konachanpic():
     page = 1
     picnum = 1
@@ -35,13 +28,33 @@ def get_konachanpic():
             picname_ = str(key)[26:999999].replace('/', '')
             picname = picname_[0:picname_.find('Konachan')]
             if not os.path.exists(os.getcwd() + '\\' + picname + '.jpg'):
-                seve_img(key, picname)
+                seve_pic(key,os.getcwd() + '\\' + picname + '.jpg')
             else:
                 print("文件已存已跳过下载")
             picnum = picnum + 1
         page = page + 1
         html = kogetli(page)
 
+
+def seve_pic(url,path):
+     start = time.time() #下载开始时间
+     response = requests.get(url, stream=True)
+     size = 0 #初始化已下载大小
+     chunk_size = 1024 # 每次下载的数据大小
+     content_size = int(response.headers['content-length']) # 下载文件总大小
+     try:
+         if response.status_code == 200: #判断是否响应成功
+          print('开始下载,[文件大小]:{size:.2f} MB'.format(size = content_size / chunk_size /1024)) #开始下载，显示下载文件大小
+          filepath = path
+          with open(filepath,'wb') as file: #显示进度条
+              for data in response.iter_content(chunk_size = chunk_size):
+               file.write(data)
+               size +=len(data)
+               print('\r'+'[下载进度]:%s%.2f%%' % ('>'*int(size*50/ content_size), float(size / content_size * 100)) ,end=' ')
+         end = time.time() #下载结束时间
+         print('下载完成！耗时: %.2f秒' % (end - start)) #输出下载用时时间
+     except:
+         print('Error!')
 
 if __name__ == '__main__':
     print("开始获取图片")

dc2849e26edd50c1872f6febda6c6930870f6da3
ERHECY <1981324730@qq.com>
2021-03-28 12:20:22 +0800

    防止出现下载到一半停止出现半张图片情况

--- konachanVer-2.py
+++ konachanVer-2.py
@@ -3,6 +3,26 @@ import re
 import os
 import time
 
+
+def get_konachanpic():
+    page = 1
+    picnum = 1
+    html = kogetli(page)
+    while True:
+        pic_url = re.findall('class="directlink largeimg" href="(.*?)"', html, re.S)
+        for key in pic_url:
+            # print(key+'\n')
+            picname_ = str(key)[26:999999].replace('/', '')
+            picname = picname_[0:picname_.find('Konachan')]
+            if not os.path.exists(os.getcwd() + '\\' + picname + '.jpg'):
+                seve_pic(key,os.getcwd() + '\\' + picname + '.jpg',picnum)
+            else:
+                print("文件已存已跳过下载")
+            picnum = picnum + 1
+        page = page + 1
+        html = kogetli(page)
+
+
 def kogetli(page):
     print('开始获取第', page, '页的图片')
     url = 'https://konachan.net/post?page=' + str(page)
@@ -16,27 +36,7 @@ def kogetli(page):
     return imglistli
 
 
-def get_konachanpic():
-    page = 1
-    picnum = 1
-    html = kogetli(page)
-    while True:
-        pic_url = re.findall('class="directlink largeimg" href="(.*?)"', html, re.S)
-        for key in pic_url:
-            # print(key+'\n')
-            print('开始获取第', picnum, '张图片')
-            picname_ = str(key)[26:999999].replace('/', '')
-            picname = picname_[0:picname_.find('Konachan')]
-            if not os.path.exists(os.getcwd() + '\\' + picname + '.jpg'):
-                seve_pic(key,os.getcwd() + '\\' + picname + '.jpg')
-            else:
-                print("文件已存已跳过下载")
-            picnum = picnum + 1
-        page = page + 1
-        html = kogetli(page)
-
-
-def seve_pic(url,path):
+def seve_pic(url,path,num):
      start = time.time() #下载开始时间
      response = requests.get(url, stream=True)
      size = 0 #初始化已下载大小
@@ -44,7 +44,7 @@ def seve_pic(url,path):
      content_size = int(response.headers['content-length']) # 下载文件总大小
      try:
          if response.status_code == 200: #判断是否响应成功
-          print('开始下载,[文件大小]:{size:.2f} MB'.format(size = content_size / chunk_size /1024)) #开始下载，显示下载文件大小
+          print('开始下载第',num,'张图片,[文件大小]:{size:.2f} MB'.format(size = content_size / chunk_size /1024)) #开始下载，显示下载文件大小
           filepath = path
           with open(filepath,'wb') as file: #显示进度条
               for data in response.iter_content(chunk_size = chunk_size):
@@ -55,6 +55,9 @@ def seve_pic(url,path):
          print('下载完成！耗时: %.2f秒' % (end - start)) #输出下载用时时间
      except:
          print('Error!')
+         os.remove(path)
+
+
 
 if __name__ == '__main__':
     print("开始获取图片")
